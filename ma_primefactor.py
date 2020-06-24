a6acd63d82ec93d1d65a60d07c75b505e5ab0424
XAPATHEIA <54541033+XAPATHEIA@users.noreply.github.com>
2020-06-24 15:57:08 +0100

    user input composite number

---
+++ ma_primefactor.py
@@ -0,0 +1,15 @@
+import time
+
+try:
+    composite_number = int(input("What number would you like to prime factorize?\n"))
+else:
+    print("Not integer, try again.")
+
+
+# trial division
+#start_time = time.time()
+#base_primes = [2, 3, 4, 7]
+#for i+1 in range(composite_number):
+
+
+#print(f"--- {(time.time() - start_time)} seconds ---")
