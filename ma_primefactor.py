2dbfd62e18682f60f99f40bcb0cfbd078b81f5c9
XAPATHEIA <54541033+XAPATHEIA@users.noreply.github.com>
2020-06-25 16:01:52 +0100

    error catching

--- ma_primefactor.py
+++ ma_primefactor.py
@@ -2,14 +2,16 @@ import time
 
 try:
     composite_number = int(input("What number would you like to prime factorize?\n"))
-else:
-    print("Not integer, try again.")
+except ValueError as v_err:
+    print(f"Not integer, try again. \n {v_err}")
+except Exception as err:
+    print(f"Other error occurred: {err}")
 
 
 # trial division
-#start_time = time.time()
-#base_primes = [2, 3, 4, 7]
-#for i+1 in range(composite_number):
+start_time = time.time()
+base_primes = [2, 3, 4, 7]
+for i + 1 in range(composite_number):
 
 
 #print(f"--- {(time.time() - start_time)} seconds ---")
